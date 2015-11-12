4e9089615c07612d7a0f94d0399f1de86b838f51
unknown <bmj29@254-5.cslabs.cs.uakron.edu>
2015-11-12 15:24:24 -0500

    Added new console project to add movies to db (to be implemented)

---
+++ MovieMadness/FillMovieDatabase/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FillMovieDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO: traverse through movies and add to our db
+            Console.Read();
+        }
+    }
+}

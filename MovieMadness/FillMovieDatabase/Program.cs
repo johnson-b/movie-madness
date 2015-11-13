611f9ea1c544a6658263fba1dd08f0abe8a42a29
unknown <bmj29@241-1.cslabs.cs.uakron.edu>
2015-11-13 15:18:57 -0500

    more work on move db

--- MovieMadness/FillMovieDatabase/Program.cs
+++ MovieMadness/FillMovieDatabase/Program.cs
@@ -4,9 +4,12 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using TMDbLib.Client;
-using TMDbLib.Objects.General;
-using TMDbLib.Objects.Movies;
+//using TMDbLib.Client;
+//using TMDbLib.Objects.General;
+//using TMDbLib.Objects.Movies;
+using System.Net.TMDb;
+using System.Threading;
+using System.IO;
 
 namespace FillMovieDatabase
 {
@@ -19,17 +22,50 @@ namespace FillMovieDatabase
                 SqlConnection connection = new SqlConnection(Properties.Settings.Default.connectionString);
                 connection.Open();
 
-                TMDbClient client = new TMDbClient(Properties.Settings.Default.tmdbApiKey);
-                SearchContainer<MovieResult> movies = client.GetMovieList(TMDbLib.Objects.Movies.MovieListType.TopRated, 100);
-                foreach (MovieResult movieResult in movies.Results)
+                TMDb db = new TMDb();
+                var moviesTask = db.GetMovies(CancellationToken.None);
+                Task.WaitAll(moviesTask);
+                List<Movie> movies = moviesTask.Result;
+                foreach (Movie movie in movies)
                 {
-                    Movie movie = client.GetMovie(movieResult.Id);
-                    Credits credits = movie.Credits;
-                    if (credits != null)
-                    {
-                        Console.WriteLine("Title: {0}", movie.Title);
-                    }
-                    
+                    SqlCommand command = connection.CreateCommand();
+
+                    /* movie table 
+                     * title
+                     * release year(int)
+                     * duration(int)
+                     * rating(R, PG etc.)
+                     */
+
+                    /* director table 
+                     * first name
+                     * last name
+                     */
+
+                    /* directMovie table
+                     * director id
+                     * movie id
+                     */
+
+                    /* actor table 
+                     * first name
+                     * last name 
+                     */
+
+                    /* actorRole table
+                     * actor id
+                     * movie id
+                     * actor_role (character name)
+                     */
+
+                    /* genre table 
+                     * genre 
+                     */
+
+                    /* movieGenre table 
+                     * genre id 
+                     * movie id
+                     */
                 }
 
                 connection.Close();
@@ -38,8 +74,27 @@ namespace FillMovieDatabase
             {
                 Console.WriteLine(e.ToString());
             }
-            
+
             Console.Read();
+        }
+    }
+
+    public class TMDb
+    {
+        public async Task<List<Movie>> GetMovies(CancellationToken cancellationToken)
+        {
+            List<Movie> movieResults = new List<Movie>();
+            using (var client = new ServiceClient(Properties.Settings.Default.tmdbApiKey))
+            {
+
+                var movies = await client.Movies.GetPopularAsync(null, 1, cancellationToken);
+                foreach (Movie m in movies.Results)
+                {
+                    var movie = await client.Movies.GetAsync(m.Id, null, true, cancellationToken);
+                    movieResults.Add(movie);
+                }
+            }
+            return movieResults;
         }
     }
 }
